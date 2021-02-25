ea78f221e17efeeb20e0ebcf3039fdf887a774da
윤재상 <banana2692@daum.net>
2021-02-26 08:04:54 +0900

    Update ClassificationPolicy

--- defs.py
+++ defs.py
@@ -24,8 +24,8 @@ class ClassificationPolicies:
 
         class SelfIncome(IncomePolicy):
             _name = '자체수입'
-            _note_regex = ['.*윤재상.*']
-            _note_filter = ['농협0125257']
+            # _note_regex = ['.*윤재상.*']
+            _note_filter = ['농협윤재상', '신한윤재상', '체크카드캐쉬백', '비씨대금환급']
 
     class Loss(ClassificationModel):
         class FixedSpending(LossPolicy):
@@ -38,12 +38,12 @@ class ClassificationPolicies:
 
         class Shopping(LossPolicy):
             _name = '쇼핑'
-            _note_filter = ['11번가', '장미가구사', '천냥앤디씨', '롯데하이마트(주)', '롯데역사(주)대구']
+            _note_filter = ['11번가', '장미가구사', '천냥앤디씨', '롯데하이마트(주)', '롯데역사(주)대구', '옥션윤재상      ']
 
         class Consumption(LossPolicy):
             _name = '소비'
             _note_regex = ['(지에스|GS)25( 불로|드림병원|달서동화)점', '씨유(대구불로대로|밀양무안점|수성롯데캐슬)?', '(팔공)?(E-|이)마트(24 S대구은)?', '다이소대구이시']
-            _note_filter = ['대백마트 (불로)']
+            _note_filter = ['대백마트 (불로)', 'AM픽쳐스', '(주)코리아세븐달']
 
         class Playing(LossPolicy):
             _name = '놀이'
@@ -55,7 +55,7 @@ class ClassificationPolicies:
 
         class EatOut(LossPolicy):
             _name = '외식'
-            _note_filter = ['(주)신세계푸드', '한상바다', '진배기원조할매국', '할리스 봉무공원', '(주)난성/롯데리']
+            _note_filter = ['(주)신세계푸드', '한상바다', '진배기원조할매국', '할리스 봉무공원', '(주)난성/롯데리', '청도새마을휴게소']
 
         class Education(LossPolicy):
             _name = '교육'
@@ -63,6 +63,7 @@ class ClassificationPolicies:
 
         class BuyForeignExchange(LossPolicy):
             _name = '외화매수'
+            _note_filter = ['윤재상(537101106']
 
         class PersonalMoneyGive(LossPolicy):
             _name = '사적금전인도'
@@ -70,13 +71,14 @@ class ClassificationPolicies:
 
         class SelfLost(LossPolicy):
             _name = '자체지출'
-            _note_regex = ['.*윤재상.*']
-            _note_filter = ['카카오페이　　　']
+            # _note_regex = ['.*윤재상.*']
+            # _note_filter = []
+            _note_filter = ['농협윤재상', '카카오페이　　　']
 
         class Fee(LossPolicy):
             _name = '수수료'
             _note_regex = ['\*{5} \d{4}년 \d{2}월 영플러스통장 수수료 면']
-            _note_filter = []
+            _note_filter = ['공공기관', '법원행정처', 'OTP발급수수료', '현금카드발급']
 
 
 
@@ -87,37 +89,8 @@ special_exceptions = {
     '2021-02-03 18:20:48': 'L사적금전인도',
     '2021-02-05 13:28:57': 'L외식',
     '2021-02-25 08:55:07': 'L소비',
+    '2021-02-02 19:31:21': 'L교육'
 }
 
 analysis_start_date = '2020-10'
 analysis_end_date = '2021-02'
-
-'''     convenience =  ['씨유', 'GS25', '이마트24', '지에스25', '코리아세븐달'],
-        'mart': ['대백마트', '천냥앤디씨', '팔공E-마트', '다이소대구이시'],
-        'shopping': ['옥션윤재상      ', '11번가', '롯데하이마트(주)', '롯데역사(주)대구', '장미가구사', '토스페이먼츠(주)'],
-
-        'trail': ['코레일유통(주)대', '한국철도공사'],
-        'taxi': ['(주)이비카드 택', '(주)이비카드택시'],
-        'bus and metro': ['모바일 티머니 충'],
-
-        'monthly': ['한화생명02045', 'KT4333557702', '대성 김윤자', '전기요금인터넷'],
-        'salary': ['급여변동상여', '급여연차보상금', '설고정상여금', '(주)대구은행'],
-        'hair': ['헤어아트#', 'DL HAIR', '아람머리방'],
-        'laundry': ['휴포레명품크리닝'],
-
-        'eat out': ['진배기원조할매국', '(주)신세계푸드', '(주)난성/롯데리', '청도새마을휴게소', '한상바다'],
-        'room charge': ['(주)인터파크홀딩', '(주)마루홀딩스 3'],
-        'drink': ['할리스 봉무공원'],
-
-        'sports': ['(주)글로벌스포츠'],
-        'pc room': ['앤유피씨(NU PC)'],
-        'education': ['한국금융투자협회'],
-        'award': ['우리(재)한국장학재단', '대구소프트웨', '국민0046255'],
-
-        'USD hedge': ['윤재상(537101106', '537101106608'],
-        'USD unhedge': ['외화적립지급'],
-        'office work': ['OTP발급수수료', '법원행정처', '공공기관', '현금카드발급', 'AM픽쳐스'],
-        'transfer to another bank': ['토스＿윤재상\u3000\u3000', '농협윤재상', '카카오페이'],
-        'personal transaction': ['농협김태윤', '토스＿김길자', '토스＿김정오', '농협윤여웅', '농협백승민', '농협김윤자', '농협0125257'],
-        'other_loss': None,
-        'other_income': ['영플러스통장 수수료 면', '현금IC캐쉬백', '체크카드캐쉬백', '신한분홍앵두', '카뱅도쿄수박', '카뱅둥근바다', '신한달콤상추', '토스180'],'''
